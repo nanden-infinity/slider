91b8c026e087dfb6422a4a821591c828ee621e71
nanden-infinity <nanden-infinity@hotmail.com>
2025-10-28 04:29:37 +0000

    Eventos de touch Adicionado

--- js/slide.js
+++ js/slide.js
@@ -6,33 +6,49 @@ export default class Slide {
   }
 
   onStart(event) {
-    event.preventDefault();
-    this.distance.startX = event.clientX;
-    this.wrapper.addEventListener("mousemove", this.onMove);
+    let mouseType;
+    if (event.type === "mousedown") {
+      event.preventDefault();
+      this.distance.startX = event.clientX;
+      mouseType = "mousemove";
+    } else {
+      this.distance.startX = Math.floor(event.changedTouches[0].clientX);
+
+      mouseType = "touchmove";
+    }
+    this.wrapper.addEventListener(mouseType, this.onMove);
   }
   moveSlide(distanceX) {
     this.distance.movePosition = distanceX;
     this.slide.style.transform = `translate3d(${distanceX}px,0,0)`;
   }
   updatePosition(clientX) {
+    clientX = Math.floor(clientX);
+    console.log(clientX);
     this.distance.movement = (this.distance.startX - clientX) * 1.6;
     return this.distance.finalPosition - this.distance.movement;
   }
   onMove(event) {
     // event.preventDefault();
-
-    const finalPosition = this.updatePosition(event.clientX);
+    const pointerPosition =
+      event.type === "mousemove"
+        ? event.clientX
+        : event.changedTouches[0].clientX;
+    const finalPosition = this.updatePosition(pointerPosition);
 
     this.moveSlide(finalPosition);
   }
-  onEnd() {
-    this.wrapper.removeEventListener("mousemove", this.onMove);
+  onEnd(event) {
+    const moveType = event.type === "mouseup" ? "mousemove" : "touchmove";
+    this.wrapper.removeEventListener(moveType, this.onMove);
 
     this.distance.finalPosition = this.distance.movePosition;
   }
   addSlideEvents() {
     this.wrapper.addEventListener("mousedown", this.onStart);
+    this.wrapper.addEventListener("touchstart", this.onStart);
     this.wrapper.addEventListener("mouseup", this.onEnd);
+    this.wrapper.addEventListener("touchend", this.onEnd);
   }
 
   bindEvents() {
